9ea3792048271d5eae990e9c7644deffa69a7997
Michaelu670 <michaelu670@gmail.com>
2023-01-20 13:59:43 +0700

    add random, file output

--- src/solution.cpp
+++ src/solution.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 string daftarOperasi = "+-/*";
+string kartuValid[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 map<string, int> daftarNilaiKartu;
 
 const int JENIS_KARTU = 13;
+const double EPS = 1e-6;
 
 void init() {
     // kartu angka 2-9
@@ -25,14 +27,24 @@ void init() {
 
     // kartu as
     daftarNilaiKartu["A"] = 1;
+
+    // randomize
+    srand(time(NULL));
+}
+
+string randomCard() {
+    return kartuValid[rand() % JENIS_KARTU];
 }
 
 bool isKartuValid(string c) {
-    string kartuValid[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     for(auto x : kartuValid) {
         if(c == x) return true;
     }
     return false;
+}
+
+bool isSame (double a, double b) {
+    return (a < b + EPS) && (b < a + EPS);
 }
 
 double operate(double a, double b, char op) {
@@ -48,31 +60,32 @@ void hitungJawaban(vector<char> &op, vector<int> &nilaiKartu, vector<string> &an
     double a = nilaiKartu[0], b = nilaiKartu[1], c = nilaiKartu[2], d = nilaiKartu[3];
     // Lakukan seluruh kemungkinan urutan
     // ((a op b) op c) op d
-    if ((int)operate(operate(operate(a, b, op[0]), c, op[1]), d, op[2]) == 24) {
+    if (isSame(operate(operate(operate(a, b, op[0]), c, op[1]), d, op[2]), 24)) {
         stringstream ss;
         ss << "((" << a << " " << op[0] << " " << b << ") " << op[1] << " " << c << ") " << op[2] << " " << d << "";
         ans.push_back(ss.str());
     }
     // (a op (b op c)) op d
-    if ((int)operate(operate(a, operate(b, c, op[1]), op[0]), d, op[2]) == 24) {
+    if (isSame(operate(operate(a, operate(b, c, op[1]), op[0]), d, op[2]), 24)) {
         stringstream ss;
         ss << "(" << a << " " << op[0] << " (" << b << " " << op[1] << " " << c << ")) " << op[2] << " " << d << "";
         ans.push_back(ss.str());
     }
     // a op (b op (c op d))
-    if ((int)operate(a, operate(b, operate(c, d, op[2]), op[1]), op[0]) == 24) {
-        stringstream ss;
-        ss << "" << a << " " << op[0] << " (" << b << " " << op[1] << " (" << c << " " << op[2] << " " << d << "))";
-        ans.push_back(ss.str());
-    }
+    // tidak perlu karena sama dengan ((a op b) op c) op d
+    // if (isSame(operate(a, operate(b, operate(c, d, op[2]), op[1]), op[0]), 24)) {
+    //     stringstream ss;
+    //     ss << "" << a << " " << op[0] << " (" << b << " " << op[1] << " (" << c << " " << op[2] << " " << d << "))";
+    //     ans.push_back(ss.str());
+    // }
     // a op ((b op c) op d)
-    if ((int)operate(a, operate(operate(b, c, op[1]), d, op[2]), op[0]) == 24) {
+    if (isSame(operate(a, operate(operate(b, c, op[1]), d, op[2]), op[0]), 24)) {
         stringstream ss;
         ss << "" << a << " " << op[0] << " ((" << b << " " << op[1] << " " << c << ") " << op[2] << " " << d << ")";
         ans.push_back(ss.str());
     }
     // (a op b) op (c op d)
-    if ((int)operate(operate(a, b, op[0]), operate(c, d, op[2]), op[1]) == 24) {
+    if (isSame(operate(operate(a, b, op[0]), operate(c, d, op[2]), op[1]), 24)) {
         stringstream ss;
         ss << "(" << a << " " << op[0] << " " << b << ") " << op[1] << " (" << c << " " << op[2] << " " << d << ")";
         ans.push_back(ss.str());
@@ -109,29 +122,76 @@ void permutasiNilaiKartu(int idx, vector<int> nilaiKartu, vector<int> jumlahKart
 
 int32_t main(int argc, char *argv[]) {
     init();
-    string path_prefix = "../test/";
-    if(argc == 2) {
-        string path_output = path_prefix;
-        path_output.append(argv[1]);
-        freopen(path_output.c_str(), "w", stdout);
+    string path_prefix = "test/";
+    bool strictCommandOutput = false;
+    if(argc == 2 || argc > 3) {
+        cout << "Invalid Argument count!" << endl;
+        return 0;
     }
     else if(argc == 3) {
         string path_input = path_prefix, path_output = path_prefix;
         path_input.append(argv[1]);
         path_output.append(argv[2]);
-        freopen(path_input.c_str(), "r", stdin);
-        freopen(path_output.c_str(), "w", stdout);
+        if (strcmp(argv[1], "cin")) {
+            freopen(path_input.c_str(), "r", stdin);
+        }
+        if (strcmp(argv[2], "cout")) {
+            freopen(path_output.c_str(), "w", stdout);
+        }
+        strictCommandOutput = true;
     }
+
+    // Input
     vector<string> kartu(4);
     vector<int> jumlahKartu(JENIS_KARTU + 1, 0);
-    for(int i = 0; i < 4; i++) {
-        cin >> kartu[i];
-        assert(isKartuValid(kartu[i]));
-        jumlahKartu[daftarNilaiKartu[kartu[i]]]++;
+    bool isRandom = false;
+    while (true) {
+        jumlahKartu = vector<int>(JENIS_KARTU + 1, 0);
+        string input_string;
+        getline(cin, input_string);
+        stringstream input_stream(input_string);
+        
+        bool invalid_input = false;
+        for(int i = 0; i < 4; i++) {
+            if (!(input_stream >> kartu[i])) {
+                // validasi input < 4
+                invalid_input = true;
+                break;
+            }
+            if (kartu[i] == "*") {
+                // random kartu
+                kartu[i] = randomCard();
+                isRandom = true;
+            }
+            if (!isKartuValid(kartu[i])) {
+                // validasi input bukan jenis kartu
+                invalid_input = true;
+                break;
+            }
+            jumlahKartu[daftarNilaiKartu[kartu[i]]]++;
+        }
+
+        string tmp;
+        if (invalid_input || input_stream >> tmp) {
+            cout << "Masukan tidak sesuai\n";
+            continue;
+        }
+
+        // lolos seluruh validasi, keluar dari loop
+        break;
     }
+
+    if (isRandom) {
+        cout << "Kartu : " << kartu[0] << ' ' << kartu[1] << ' ' << kartu[2] << ' ' << kartu[3] << '\n';
+    }
+
+    // Selesai input, mulai menghitung waktu eksekusi program
+    auto start = chrono::high_resolution_clock::now();
 
     vector<string> ans;
     permutasiNilaiKartu(0, vector<int>(), jumlahKartu, ans);
+
+    auto end = chrono::high_resolution_clock::now();
 
     if (ans.size() == 0) {
         cout << "no solution found\n";
@@ -145,5 +205,49 @@ int32_t main(int argc, char *argv[]) {
     
     for(string s: ans) {
         cout << s << '\n';
+    }
+
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+    cout << "Execution time = " << duration.count() << " microseconds" << endl;
+
+    if (!strictCommandOutput) {
+        while(true) {
+            cout << "Apakah ingin menyimpan solusi?(Y/N)\n";
+            string response;
+            cin >> response;
+            if (response == "Y" || response == "y") {
+                cout << "Nama file (contoh: testcase3.out) : ";
+                string nama_file;
+                cin >> nama_file;
+                string path_output = path_prefix + nama_file;
+                ofstream output_stream(path_output);
+
+                if (isRandom) {
+                    output_stream << "Kartu : " << kartu[0] << ' ' << kartu[1] << ' ' << kartu[2] << ' ' << kartu[3] << '\n';
+                }
+
+                if (ans.size() == 0) {
+                    output_stream << "no solution found\n";
+                }
+                else if (ans.size() == 1) {
+                    output_stream << ans.size() << " solution found\n";
+                }
+                else {
+                    output_stream << ans.size() << " solutions found\n";
+                }
+                
+                for(string s: ans) {
+                    output_stream << s << '\n';
+                }
+
+                cout << "Solusi sudah dimasukkan ke " << path_output << '\n';
+                break;
+            }
+            else if (response == "N" || response == "n") {
+                break;
+            }
+            
+        }
+        
     }
 }
